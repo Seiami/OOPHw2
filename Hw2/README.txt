be67a20b1e10a1d16b96e6b14c268c71d560536a
Raquel F <noodlesthebird@gmail.com>
2026-03-20 19:06:30 -0500

    Finished quest board, added read me

---
+++ Hw2/README.txt
@@ -0,0 +1,5 @@
+Team Members: Raquel Fellman, Charles Bacher, Anyshya Hemphill
+
+Raquel: implemented Quest Interface, QuestBoard, Main
+Charles: implemented all concrete quest classes, Student
+Anyshya: implemented
--- Hw2/src/edu/mu/cs330/assign2/QuestBoard.java
+++ Hw2/src/edu/mu/cs330/assign2/QuestBoard.java
@@ -1,5 +1,6 @@
 package edu.mu.cs330.assign2;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -7,35 +8,60 @@ public class QuestBoard {
 	private Map<Integer, Quest> questsById;
 	private Map<Student, List<Quest>> assignments;
 	
-	// Need constructor?
-//	public QuestBoard() {
-//	
-//	}
-	
 	/*
 	 * Function to add a quest to the quest board.
 	 * Rejects duplicate quest IDs by throwing exception.
 	 * Takes a quest to be added and returns nothing.
 	 */
 	public void addQuest(Quest q) {
-		// Return smth?
+		int id = q.getID();
+		// Check for duplicate IDs
+		if (questsById.containsKey(id)) {
+			throw new IllegalArgumentException("Attempted to add already included quest.");
+		}
+		questsById.put(id, q);
+		
+		return;
 	}
 	
 	/*
 	 * Function to find a quest given its ID.
-	 * Returns the quest or NULL if it does not exist on the board.
+	 * Returns the quest or null if it does not exist on the board.
 	 */
 	public Quest findQuest(int id) {
-		
+		if (questsById.containsKey(id)) {
+			return questsById.get(id);
+		}
+		return null;
 	}
 	
 	/*
 	 * Function to a assign a quest to a student.
 	 * Takes an existing student and the ID of the quest to be assigned.
-	 * DECIDE!!! Can the same quest can be assigned multiple times to the same student?
+	 * The same quest CANNOT be assigned multiple times to the same student.
 	 */
 	public void assignQuest(Student s, int questID) {
-		// return smth?
+		Quest q = findQuest(questID);
+		// Ensure quest is in list
+		if (q == null) {
+			throw new IllegalArgumentException("Attempted to assign unregistered quest ID.");
+		}
+		
+		// Check if student is in assignments
+		if (!assignments.containsKey(s)) {
+			assignments.put(s, new ArrayList<Quest>());
+			assignments.get(s).add(q);
+			return;
+		}
+		// Check if student has already been assigned this quest
+		if (assignments.get(s).contains(q)) {
+			throw new IllegalArgumentException("Student has already been assigned this quest.");
+		}
+		else {
+			assignments.get(s).add(q);
+		}
+		
+		return;
 	}
 	
 	/*
@@ -44,14 +70,26 @@ public class QuestBoard {
 	 * Returns the awarded points (integer).
 	 */
 	public int completeQuest(Student s, int questID) {
-		
+		// Check if student is registered
+		if (!assignments.containsKey(s)) {
+			throw new IllegalArgumentException("Tried to complete quest, but student is not registered.");
+		}
+		Quest q = findQuest(questID);
+		// Check if quest is assigned
+		if (!assignments.get(s).contains(q)) { 
+			throw new IllegalArgumentException("Tried to complete quest for student, but student is not assigned the quest.");
+		}
+		return q.completeFor(s);
 	}
 	
 	/*
-	 * Function to print all quests on the questboard.
+	 * Function to print all quests on the quest board.
 	 */
 	public void printAllQuests() {
-		
+		for (Map.Entry<Integer, Quest> entry : questsById.entrySet()) {
+			entry.getValue().toString(); // Print Quest
+		}
+		return;
 	}
 	
 	/*
@@ -59,6 +97,13 @@ public class QuestBoard {
 	 * Takes a student s.
 	 */
 	public void printAssignmentsFor(Student s) {
-		
+		if (assignments.containsKey(s)) {
+			System.out.println(s.getName() + "'s Quests:");
+			List<Quest> list = assignments.get(s);
+			for (Quest q : list) {
+				q.toString();
+			}
+		}
+		return;
 	}
 }
